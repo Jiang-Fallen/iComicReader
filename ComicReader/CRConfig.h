c9001420312af400c8f98b8fa5c3128c2e095604
zhaoqiang <zhaoqiang@zhongan.com>
2018-07-11 19:09:14 +0800

    更换请求地址，增加iPhoneX适配

---
+++ ComicReader/CRConfig.h
@@ -0,0 +1,13 @@
+//
+//  CRConfig.h
+//  ComicReader
+//
+//  Created by M Jiang on 2018/7/11.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#ifndef CRConfig_h
+#define CRConfig_h
+
+
+#endif /* CRConfig_h */
